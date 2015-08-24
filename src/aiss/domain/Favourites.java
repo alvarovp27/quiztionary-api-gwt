703c962f2a1cdbe0696024d79e7d627a61e25d9f
alvarovp27 <avalenciaparra@gmail.com>
2015-08-24 23:24:07 +0200

    URI para comprobar si un token ha expirado y primera carga de palabras

--- src/aiss/domain/Favourites.java
+++ src/aiss/domain/Favourites.java
@@ -29,5 +29,38 @@ public class Favourites {
 	public void setUser(String user) {
 		this.user = user;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		result = prime * result + ((word == null) ? 0 : word.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Favourites other = (Favourites) obj;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		if (word == null) {
+			if (other.word != null)
+				return false;
+		} else if (!word.equals(other.word))
+			return false;
+		return true;
+	}
+	
+	
 	
 }
